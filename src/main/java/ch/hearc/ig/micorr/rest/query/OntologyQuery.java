a32a4b26cbd7344333a50c226ee8a16a75ae2c2c
jerome.schneide <jerome.schneider@he-arc.ch>
2018-07-30 21:24:29 +0200

    Mise à jour de la manière de se connecter au serveur Fuseki suite aux problèmes de mémoire rencontrés

--- src/main/java/ch/hearc/ig/micorr/rest/query/OntologyQuery.java
+++ src/main/java/ch/hearc/ig/micorr/rest/query/OntologyQuery.java
@@ -6,48 +6,53 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
-import org.apache.jena.rdfconnection.RDFConnectionFuseki;
-import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
 public class OntologyQuery {
 
 	private static final String FILENAME1 = "C:\\DEV\\SPARQL\\micorr_query1.txt";
-	
-	private static final boolean CONSOLE_LOG = true;
+	private static final String FILENAME2 = "C:\\DEV\\SPARQL\\micorr_query2.txt";
 
-	
-
-	private RDFConnectionRemoteBuilder builder;
-	
-	
 	public OntologyQuery() {
-    	builder = RDFConnectionFuseki.create()
-                .destination("http://localhost:8080/MiCorrDS/")
-                .gspEndpoint("MiCorrGraph");
 	}
 
 	public List<QuerySolution> getPropertiesDataQuery(String text) {
 		String sparqlRequest = readFileToString(FILENAME1);
-		
-		sparqlRequest.replace("%text", text);
 
-		System.out.println(sparqlRequest);
+		sparqlRequest = sparqlRequest.replaceAll("%text%", text);
 
 		Query query = QueryFactory.create(sparqlRequest);
 
+		return queryExec(query);
+	}
+
+	private static List<QuerySolution> queryExec(Query query) {
 		List<QuerySolution> list = null;
+		
+		// TODO Mettre l'adresse du serveur dans le fichier de configuration YAML avec
+		// les requêtes
+		try (QueryExecution qexec = QueryExecutionFactory.sparqlService("http://localhost:8080/MiCorrDS/", query)) {
+			// Set the DBpedia specific timeout.
+			((QueryEngineHTTP) qexec).addParam("timeout", "10000");
 
-		// In this variation, a connection is built each time.
-		try (RDFConnectionFuseki conn = (RDFConnectionFuseki) builder.build()) {
-			if(CONSOLE_LOG) {
-				conn.queryResultSet(query, ResultSetFormatter::out);
-			}
-			list = ResultSetFormatter.toList(conn.query(query).execSelect());
+			// Execute.
+			ResultSet rs = qexec.execSelect();
+			// ResultSet rsOut = qexec.execSelect();
+
+			list = ResultSetFormatter.toList(rs);
+			// ResultSetFormatter.out(System.out, rsOut, query);
+
+			qexec.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
+		
 		return list;
 	}
 
